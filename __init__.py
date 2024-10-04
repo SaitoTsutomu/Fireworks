3a3f239394cee9fe55694113b0b7c2f9c0b9cda2
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2024-10-05 08:54:37 +0900

    ver:0.2, blender 4.2

--- __init__.py
+++ __init__.py
@@ -1,19 +1,20 @@
 import re
+from pathlib import Path
 
 import bpy
-import yaml
+import tomllib
 
 bl_info = {
     "name": "Fireworks",
     "author": "tsutomu",
-    "version": (0, 1),
-    "blender": (3, 1, 0),
+    "version": (0, 2),
+    "blender": (4, 2, 0),
     "support": "TESTING",
     "category": "Object",
     "description": "",
     "location": "View3D > Sidebar > Edit Tab",
     "warning": "",
-    "doc_url": "https://github.com/SaitoTsutomu/Fireworks",  # ドキュメントURL
+    "doc_url": "https://github.com/SaitoTsutomu/Fireworks",
 }
 
 
@@ -23,20 +24,20 @@ def make_material(st, name, attr):
     mat.blend_method = "HASHED"
     bsdf = mat.node_tree.nodes["Principled BSDF"]
     if "color_ramp" in attr:
-        clrm = mat.node_tree.nodes.new(type="ShaderNodeValToRGB")
+        col_rmp = mat.node_tree.nodes.new(type="ShaderNodeValToRGB")
         attr_elms = attr.get("color_ramp", {})
-        for _ in range(len(clrm.color_ramp.elements), len(attr_elms)):
-            clrm.color_ramp.elements.new(position=0)
-        for element, attr_elm in zip(clrm.color_ramp.elements, attr_elms):
+        for _ in range(len(col_rmp.color_ramp.elements), len(attr_elms)):
+            col_rmp.color_ramp.elements.new(position=0)
+        for element, attr_elm in zip(col_rmp.color_ramp.elements, attr_elms):
             element.position = attr_elm.get("position", 0)
             element.color = attr_elm.get("color", [1, 1, 1, 1])
-        mat.node_tree.links.new(clrm.outputs["Color"], bsdf.inputs["Emission"])
-        mat.node_tree.links.new(clrm.outputs["Alpha"], bsdf.inputs["Alpha"])
-        drv = clrm.inputs["Fac"].driver_add("default_value").driver
+        mat.node_tree.links.new(col_rmp.outputs["Color"], bsdf.inputs["Emission Color"])
+        mat.node_tree.links.new(col_rmp.outputs["Alpha"], bsdf.inputs["Alpha"])
+        drv = col_rmp.inputs["Fac"].driver_add("default_value").driver
         val = st.frame_end - st.frame_start + st.lifetime
         drv.expression = f"(frame - {st.frame_start}) / {val}"
     else:
-        bsdf.inputs["Emission"].default_value = attr.get("color", [1, 1, 1, 1])
+        bsdf.inputs["Emission Color"].default_value = attr.get("color", [1, 1, 1, 1])
     bsdf.inputs["Emission Strength"].default_value = attr.get("strength", 10)
     return mat
 
@@ -58,14 +59,32 @@ def make_spark(st, name, attr):
     return spark
 
 
+def set_bloom():
+    scene = bpy.data.scenes["Scene"]
+    scene.use_nodes = True
+    nodes = scene.node_tree.nodes
+    glare = nodes.new(type="CompositorNodeGlare")
+    glare.glare_type = "BLOOM"
+    glare.quality = "LOW"
+    glare.location = 50, 200
+    node1 = scene.node_tree.nodes["Render Layers"]
+    node2 = scene.node_tree.nodes["Composite"]
+    scene.node_tree.links.new(node1.outputs[0], glare.inputs[0])
+    scene.node_tree.links.new(glare.outputs[0], node2.inputs[0])
+    for area in bpy.data.screens["Layout"].areas:
+        if area.ui_type == "VIEW_3D":
+            area.spaces[0].shading.type = "RENDERED"
+            area.spaces[0].shading.use_compositor = "ALWAYS"
+
+
 class CFW_OT_make_fireworks(bpy.types.Operator):
     bl_idname = "object.make_fireworks"
     bl_label = "Make"
     bl_description = ""
 
-    filepath: bpy.props.StringProperty()  # type: ignore # noqa
+    filepath: bpy.props.StringProperty()  # type: ignore[valid-type]
 
-    def execute(self, context):
+    def execute(self, context):  # noqa: PLR0915
         context.scene.eevee.use_bloom = True
         if "fireworks" in bpy.data.collections:
             bpy.data.collections.remove(bpy.data.collections["fireworks"])
@@ -74,9 +93,8 @@ class CFW_OT_make_fireworks(bpy.types.Operator):
         bpy.context.scene.collection.children.link(col)
         lc = bpy.context.view_layer.layer_collection.children["fireworks"]
         bpy.context.view_layer.active_layer_collection = lc
-        with open(self.filepath) as fp:
-            yml = yaml.safe_load(fp)
-        for name, attr in yml.items():
+        toml = tomllib.loads((Path(__file__).parent / self.filepath).read_text())
+        for name, attr in toml.items():
             bpy.ops.mesh.primitive_ico_sphere_add(
                 subdivisions=1,
                 radius=attr.get("radius", 0.1),
@@ -103,27 +121,30 @@ class CFW_OT_make_fireworks(bpy.types.Operator):
             obj.location = list(obj.location * 2 - launch_location)[:2] + [launch_location[2]]
             obj.keyframe_insert(data_path="location", frame=disappear)
             ptn = r"(launch|explode)\s([+-])\s*(\d+)\s*(?:|#.*)$"
-            for psname, psattr in attr.get("particle_systems", {}).items():
+            for ps_name, ps_attr in attr.get("particle_systems", {}).items():
                 bpy.ops.object.particle_system_add()
                 ps = obj.particle_systems[-1]
-                ps.name = psname
+                ps.name = ps_name
                 st = ps.settings
-                st.count = psattr["count"]
-                m = re.match(ptn, psattr["frame_start"])
+                st.count = ps_attr["count"]
+                m = re.match(ptn, ps_attr["frame_start"])
                 bs = {"launch": launch, "explode": explode}[m.group(1)]
                 st.frame_start = bs + ((m.group(2) == "+") * 2 - 1) * int(m.group(3))
-                m = re.match(ptn, psattr["frame_end"])
+                m = re.match(ptn, ps_attr["frame_end"])
                 bs = {"launch": launch, "explode": explode}[m.group(1)]
                 st.frame_end = bs + ((m.group(2) == "+") * 2 - 1) * int(m.group(3))
-                st.lifetime = psattr["lifetime"]
+                st.lifetime = ps_attr["lifetime"]
                 st.lifetime_random = 1
                 st.render_type = "OBJECT"
-                spark = make_spark(st, f"{name}_{psname}", psattr.get("material", {}))
+                spark = make_spark(st, f"{name}_{ps_name}", ps_attr.get("material", {}))
                 st.instance_object = spark
                 st.particle_size = attr.get("particle_size", 0.05)
-                st.factor_random = psattr.get("factor_random", 0)
-                st.normal_factor = st.effector_weights.gravity = psattr.get("gravity", 1)
+                st.factor_random = ps_attr.get("factor_random", 0)
+                st.normal_factor = st.effector_weights.gravity = ps_attr.get("gravity", 1)
         bpy.context.scene.frame_set(launch)
+        set_bloom()  # ブルームの設定
+        # ワールドの背景色を黒に設定
+        bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = [0, 0, 0, 1]
         return {"FINISHED"}
 
 
@@ -152,7 +173,7 @@ classes = [
 def register():
     for c in classes:
         bpy.utils.register_class(c)
-    bpy.types.Scene.filepath = bpy.props.StringProperty(default="/tmp/fireworks.yaml")
+    bpy.types.Scene.filepath = bpy.props.StringProperty(default="./fireworks.toml")
 
 
 def unregister():
